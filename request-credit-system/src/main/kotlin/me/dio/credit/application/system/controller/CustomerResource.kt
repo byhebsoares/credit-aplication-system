276258ff6871f48e33e9a82df5ec5f644d2e41f8
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2024-02-01 19:49:07 -0300

    att

---
+++ request-credit-system/src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
@@ -0,0 +1,19 @@
+package me.dio.credit.application.system.controller
+
+import me.dio.credit.application.system.dto.CustomerDto
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/customers")
+class CustomerResource(
+    private val customerServuce: CustomerService
+) {
+    @PostMapping
+
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): String{
+
+    }
+}
---
+++ request-credit-system/src/main/kotlin/me/dio/credit/application/system/dto/CustomerDto.kt
@@ -0,0 +1,16 @@
+package me.dio.credit.application.system.dto
+
+import java.math.BigDecimal
+
+data class CustomerDto(
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val income: BigDecimal,
+    val email: String,
+    val password: String,
+    val zipCode: String,
+    val street: String
+) {
+
+}
