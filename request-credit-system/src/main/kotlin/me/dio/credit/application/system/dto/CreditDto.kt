e4889a261e9cf5fb7777753d0751ab71e484d4e0
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2024-02-04 21:19:40 -0300

    refatoação dos endpoint

--- request-credit-system/src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
+++ request-credit-system/src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
@@ -1,2 +1,20 @@
-package me.dio.credit.application.system.dto 
+package me.dio.credit.application.system.dto
 
+import me.dio.credit.application.system.entity.Credit
+import me.dio.credit.application.system.entity.Customer
+import java.math.BigDecimal
+import java.time.LocalDate
+
+data class CreditDto(
+    val creditValue: BigDecimal,
+    val dayFirstOfInstallment: LocalDate,
+    val numberOfInstalments: Int,
+    val customerId: Long,
+) {
+    fun toEntity(): Credit = Credit(
+        creditValue = this.creditValue,
+        dayFirstInstallment = this.dayFirstOfInstallment,
+        numberOfInstallments = this.numberOfInstalments,
+        customer = Customer(id = this.customerId)
+    )
+}
--- request-credit-system/src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
+++ request-credit-system/src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
@@ -1,8 +1,8 @@
 package me.dio.credit.application.system.entity
+
 import jakarta.persistence.*
 import java.math.BigDecimal
 import kotlin.collections.*
-
 
 
 @Entity
@@ -10,14 +10,16 @@ import kotlin.collections.*
 data class Customer(
     @Column(nullable = false) var firstName: String = "",
     @Column(nullable = false) var lastName: String = "",
-    @Column(nullable = false, unique = true ) val cpf: String,
+    @Column(nullable = false, unique = true) var cpf: String = "",
     @Column(nullable = false, unique = true) var email: String = "",
-    @Column(nullable = false) val income: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) var income: BigDecimal = BigDecimal.ZERO,
     @Column(nullable = false) var password: String = "",
     @Column(nullable = false) @Embedded var address: Address = Address(),
-    @Column(nullable = false) @OneToMany (fetch = FetchType.LAZY,
-    cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST),
-    mappedBy = "customer")
+    @Column(nullable = false) @OneToMany(
+        fetch = FetchType.LAZY,
+        cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST),
+        mappedBy = "customer"
+    )
     var credits: MutableList<Credit> = mutableListOf(),
-    @Id @GeneratedValue( strategy = GenerationType.IDENTITY) val id: Long? = null
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
