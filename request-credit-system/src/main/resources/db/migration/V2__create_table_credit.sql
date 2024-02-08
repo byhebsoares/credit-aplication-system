4c75daa27cc14b397f2163a722ee5df320ffbb87
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2024-02-08 19:04:38 -0300

    Tratamento de exceções

--- request-credit-system/src/main/resources/db/migration/V2__create_table_credit.sql
+++ request-credit-system/src/main/resources/db/migration/V2__create_table_credit.sql
@@ -5,10 +5,10 @@ CREATE TABLE credit (
    day_first_installment date NOT NULL,
    number_of_installments INT NOT NULL,
    status SMALLINT,
-   custumer_id BIGINT,
+   customer_id BIGINT,
    CONSTRAINT pk_credit PRIMARY KEY (id)
 );
 
 ALTER TABLE credit ADD CONSTRAINT uc_credit_credit_code UNIQUE (credit_code);
 
-ALTER TABLE credit ADD CONSTRAINT FK_CREDIT_ON_CUSTUMER FOREIGN KEY (custumer_id) REFERENCES cliente (id);
+ALTER TABLE credit ADD CONSTRAINT FK_CREDIT_ON_CUSTOMER FOREIGN KEY (customer_id) REFERENCES cliente (id);
