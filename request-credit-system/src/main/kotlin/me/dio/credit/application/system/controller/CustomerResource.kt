b1ef5bfb86022d29670b10909200a54758859a14
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2024-02-09 19:44:55 -0300

    Update README

--- request-credit-system/src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
+++ request-credit-system/src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -29,16 +30,14 @@ class CustomerResource(
         val savedCustomer = this.customerService.save(customerDto.toEntity())
         return ResponseEntity.status(HttpStatus.CREATED).body("Customer ${savedCustomer.email} saved!")
     }
-
     @GetMapping("/{id}")
     fun findById(@PathVariable id: Long): ResponseEntity<CustomerView> {
         val costomer: Customer = this.customerService.findById(id)
         return ResponseEntity.status(HttpStatus.OK).body(CustomerView(costomer))
     }
-
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
-
     @PatchMapping
     fun updateCustomer(
         @RequestParam(value = "customerId") id: Long,
